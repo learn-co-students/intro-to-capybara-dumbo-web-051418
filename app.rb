7aa8591911fe0d68801934b07a9f32236eef51b5
Razibul Ahmed <razibul.ahmed@gmail.com>
2018-06-04 16:58:36 -0400

    Completed intro-to-capybara-dumbo-web-051418

--- app.rb
+++ app.rb
@@ -1,4 +1,12 @@
 class Application < Sinatra::Base
   # Write your code here!
+  get '/' do
+    erb :index
+  end
+
+  # New route responds to the form submission
+  post '/greet' do
+    erb :greet
+  end
 
 end
